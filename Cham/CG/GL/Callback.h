f498fd5d97ca6b44159d2f0dcb0ec78461f9eba0
ChamChiCham@Laptop <kyjp0623@gmail.com>
2023-09-18 08:24:16 +0900

    분할 구현 및 싱글턴 구현

---
+++ Cham/CG/GL/Callback.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <gl/glew.h>              
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+#include "WindowMgr.h"
+
+namespace cb
+{
+	
+	GLvoid Display();
+	GLvoid Reshape(int w, int h);
+
+}
